6734c8b1d4fbfb474c4cc569952409e3bc026ff5
pablomaraya <pablo.araya@globant.com>
2016-08-10 15:27:17 -0300

    Add Test project

--- test/DDD.ApplicationService.Test/Test.cs
+++ test/DDD.ApplicationService.Test/Test.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit;
 
 namespace DDD.ApplicationService.Test
 {
-    public class Class1
+  
+    public class Test
     {
+        [Fact]
+        public void Dummy_Check()
+        {
+            Assert.True(true);
+        }
     }
 }
